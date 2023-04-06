e73a0bc15c3daea4f226811e1d896ac7793f98ac
Pavel <sdpv55@gmail.com>
2023-04-06 15:39:57 +0300

    D3.2 end. Make a separate page for a specific news at /news/<news id>. All pages are part of the default.html main template.

--- news/views.py
+++ news/views.py
@@ -1,5 +1,5 @@
 #from django.shortcuts import render
-from django.views.generic import ListView # импортируем класс, который говорит нам о том, что в этом представлении мы будем выводить список объектов из БД
+from django.views.generic import ListView, DetailView # импортируем класс, который говорит нам о том, что в этом представлении мы будем выводить список объектов из БД
 from .models import Post
 
 # Create your views here.
@@ -7,3 +7,9 @@ class PostsList(ListView):
     model = Post  # указываем модель, объекты которой мы будем выводить
     template_name = 'news.html'  # указываем имя шаблона, в котором будет лежать HTML, в нём будут все инструкции о том, как именно пользователю должны вывестись наши объекты
     context_object_name = 'posts'  # это имя списка, в котором будут лежать все объекты, его надо указать, чтобы обратиться к самому списку объектов через HTML-шаблон
+
+# создаём представление, в котором будут детали конкретного отдельного товара
+class PostsDetail(DetailView):
+    model = Post # модель всё та же, но мы хотим получать детали конкретно отдельного товара
+    template_name = 'new.html' # название шаблона будет product.html
+    context_object_name = 'post' # название объекта
