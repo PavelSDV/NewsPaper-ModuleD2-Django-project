27a9874c0f1dae3a9ae9b7b085f0d796e2f87121
Pavel <sdpv55@gmail.com>
2023-05-11 21:56:12 +0300

    Commit 38 doesn't work. Now everything works. I commented out the cache, because the application began to work incorrectly, see commit 37, due to a Sqlite error.

--- news/models.py
+++ news/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models import Sum
 from datetime import datetime
 
-from django.core.cache import cache
+# from django.core.cache import cache
 
 # from django.contrib.auth.forms import UserCreationForm
 # from django import forms
@@ -73,9 +73,9 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)  # сначала вызываем метод родителя, чтобы объект сохранился
-        cache.delete(f'post-{self.pk}')  # затем удаляем его из кэша, чтобы сбросить его
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)  # сначала вызываем метод родителя, чтобы объект сохранился
+    #     cache.delete(f'post-{self.pk}')  # затем удаляем его из кэша, чтобы сбросить его
 
 class PostCategory(models.Model):
     postThrough = models.ForeignKey(Post, on_delete=models.CASCADE)
